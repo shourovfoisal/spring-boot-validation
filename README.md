45121a0844a6fdfb0f5a20eca7fd36b412e14b93
shourov.foisal <shourov.foisal@technonext.com>
2025-07-14 14:35:29 +0600

    fix validation and handle null cases

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 **Spring Boot | REST API Request Validation & Exception Handling Realtime Example | JavaTechie**
 https://www.youtube.com/watch?v=gPnd-hzM_6A
+
+**Test with**
+
+```json
+{
+  "name": "",
+  "age": 10,
+  "email": "@gmail.com",
+  "mobile": "4256874563",
+  "gender": "Male",
+  "nationality": ""
+}
+```
--- src/main/java/com/shourov/validation_example/dto/UserRequest.java
+++ src/main/java/com/shourov/validation_example/dto/UserRequest.java
@@ -9,15 +9,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor(staticName = "build")
 @NoArgsConstructor
 public class UserRequest {
-    private int age;
+    @NotNull(message = "Age is a required field.")
+    @Min(value = 18, message = "Age below 18 is not permitted.")
+    @Max(value = 70, message = "Age above 70 is not permitted.")
+    private Integer age;
     @NotNull(message = "Name should not be null.")
     private String name;
     @Email(message = "Invalid email format.")
     private String email;
     @Pattern(regexp = "^\\d{10}$", message = "Mobile number should be ten digits.")
     private String mobile;
-    @Min(value = 18, message = "Age below 18 is not permitted.")
-    @Max(value = 70, message = "Age above 70 is not permitted.")
     private String gender;
     @NotBlank
     private String nationality;

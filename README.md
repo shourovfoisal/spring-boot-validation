34fdf5545ce654f907b410715ce27d06f402aae2
shourov.foisal <shourov.foisal@technonext.com>
2025-07-21 10:04:18 +0600

    Handle malformed request body exception

--- README.md
+++ README.md
@@ -1,12 +1,24 @@
 **Spring Boot | REST API Request Validation & Exception Handling Realtime Example | JavaTechie**
 https://www.youtube.com/watch?v=gPnd-hzM_6A
 
-**Test with**
+**Test Validation With**
 
 ```json
 {
   "name": "",
   "age": 10,
+  "email": "@gmail.com",
+  "mobile": "425687456",
+  "gender": "Male",
+  "nationality": ""
+}
+```
+
+**Test Malformed Request Body With**
+```json
+{
+  "name": "",
+  "age": "10ab",
   "email": "@gmail.com",
   "mobile": "425687456",
   "gender": "Male",
--- src/main/java/com/shourov/validation_example/advice/AppExceptionHandler.java
+++ src/main/java/com/shourov/validation_example/advice/AppExceptionHandler.java
@@ -1,13 +1,17 @@
 package com.shourov.validation_example.advice;
 
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.shourov.validation_example.exception.UserNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestControllerAdvice
@@ -15,8 +19,8 @@ public class AppExceptionHandler {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleInvalidMethodArgument(MethodArgumentNotValidException e) {
-        Map<String, String> errors = new HashMap<>();
+    public Map<String, Object> handleInvalidMethodArgument(MethodArgumentNotValidException e) {
+        Map<String, Object> errors = new HashMap<>();
         e.getBindingResult().getFieldErrors().forEach(error -> {
             errors.put(error.getField(), error.getDefaultMessage());
         });
@@ -25,9 +29,26 @@ public class AppExceptionHandler {
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(UserNotFoundException.class)
-    public Map<String, String> handleNonExistentUser(UserNotFoundException e) {
-        Map<String, String> error = new HashMap<>();
+    public Map<String, Object> handleNonExistentUser(UserNotFoundException e) {
+        Map<String, Object> error = new HashMap<>();
         error.put("message", e.getMessage());
         return error;
+    }
+    
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Map<String, Object>> handleMalformedRequestBody(HttpMessageNotReadableException e) {
+        Map<String, Object> response = new HashMap<>();
+        
+        Throwable rootCause = e.getMostSpecificCause();
+        if(rootCause instanceof JsonMappingException jsonMappingException) {
+            List<JsonMappingException.Reference> path = jsonMappingException.getPath();
+            if(!path.isEmpty()) {
+                String fieldName = path.getFirst().getFieldName();
+                response.put("reason", "Malformed data");
+                response.put("field", fieldName);
+            }
+        }
+        response.put("details", e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 }
